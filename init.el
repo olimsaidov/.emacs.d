e1d8310f4885ec74f13b75d0c5d3b2c6ed87dd73
Olim Saidov <olim.mail@gmail.com>
2015-12-20 14:21:16 +0500

    First commit

---
+++ init.el
@@ -0,0 +1,5 @@
+;;; init.el - Olim's emacs configuration file
+
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
